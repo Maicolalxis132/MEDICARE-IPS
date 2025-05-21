e962bdea6bdf2c84f756b947163f81ab898879df
Medicare IPS <maicolalxis132@gmail.com>
2025-05-21 11:29:18 -0500

    Proxima actualizacion

--- Archivos/script.js
+++ Archivos/script.js
@@ -1,8 +1,24 @@
-function mostrar() {
-  const boton = document.getElementById("boton-movil");
-  const menu = document.getElementById("menu-movil");
+// function mostrar() {
+//   const boton = document.getElementById("boton-movil");
+//   const menu = document.getElementById("menu-movil");
 
-  boton.addEventListener("click", function() {
-    menu.classList.toggle("hidden");
+//   boton.addEventListener("click", function() {
+//     menu.classList.toggle("hidden");
+//   });
+// }
+
+const boton = document.getElementById('boton-movil');
+  const menu = document.getElementById('menu-movil');
+  let abierto = false;
+
+  boton.addEventListener('click', () => {
+    abierto = !abierto;
+
+    if (abierto) {
+      menu.classList.remove('max-h-0', 'opacity-0');
+      menu.classList.add('max-h-[300px]', 'opacity-100');
+    } else {
+      menu.classList.add('max-h-0', 'opacity-0');
+      menu.classList.remove('max-h-[300px]', 'opacity-100');
+    }
   });
-}
