36dc42b410f7c1032ba6539903eb99c01f9aae1b
Medicare IPS <maicolalxis132@gmail.com>
2025-05-23 14:21:35 -0500

    siguiente cambio

--- Archivos/script.js
+++ Archivos/script.js
@@ -1,11 +1,6 @@
-// function mostrar() {
-//   const boton = document.getElementById("boton-movil");
-//   const menu = document.getElementById("menu-movil");
 
-//   boton.addEventListener("click", function() {
-//     menu.classList.toggle("hidden");
-//   });
-// }
+// MENU DESPLEGABLE
+// Este código es para el menú desplegable en dispositivos móviles
 
 const boton = document.getElementById('boton-movil');
   const menu = document.getElementById('menu-movil');
@@ -22,3 +17,25 @@ const boton = document.getElementById('boton-movil');
       menu.classList.remove('max-h-[300px]', 'opacity-100');
     }
   });
+
+
+// CARRUSEL DE FOTOS
+// Este código es para el carrusel de fotos
+
+   const slider = document.querySelector("#carousel > div");
+    const totalSlides = slider.children.length;
+    let index = 0;
+
+    function showSlide() {
+      slider.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    document.getElementById("prev").addEventListener("click", () => {
+      index = (index - 1 + totalSlides) % totalSlides;
+      showSlide();
+    });
+
+    document.getElementById("next").addEventListener("click", () => {
+      index = (index + 1) % totalSlides;
+      showSlide();
+    });
