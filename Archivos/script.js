4c1a7c6ae64d202448154ac546391c242e0a26b5
Medicare IPS <maicolalxis132@gmail.com>
2025-05-31 11:46:16 -0500

    1.5

--- Archivos/script.js
+++ Archivos/script.js
@@ -22,20 +22,47 @@ const boton = document.getElementById('boton-movil');
 // CARRUSEL DE FOTOS
 // Este código es para el carrusel de fotos
 
-   const slider = document.querySelector("#carousel > div");
-    const totalSlides = slider.children.length;
-    let index = 0;
+const slider = document.querySelector("#carousel > div");
+const totalSlides = slider.children.length;
+let index = 0;
 
-    function showSlide() {
-      slider.style.transform = `translateX(-${index * 100}%)`;
+function showSlide() {
+  slider.style.transform = `translateX(-${index * 100}%)`;
+}
+
+// Botones manuales
+document.getElementById("prev").addEventListener("click", () => {
+  index = (index - 1 + totalSlides) % totalSlides;
+  showSlide();
+});
+
+document.getElementById("next").addEventListener("click", () => {
+  index = (index + 1) % totalSlides;
+  showSlide();
+});
+
+// Cambio automático cada 3 segundos
+setInterval(() => {
+  index = (index + 1) % totalSlides;
+  showSlide();
+}, 3000); // 3000 milisegundos = 3 segundos
+
+  const card = document.getElementById("menu-card");
+  const icon = document.getElementById("menu-icon");
+  const content = document.getElementById("menu-content");
+
+  let menuVisible = false;
+
+  function toggleMenu() {
+    // Solo aplica en xs: (798px o más)
+    if (window.innerWidth >= 798) {
+      menuVisible = !menuVisible;
+      content.style.transform = menuVisible ? "scaleY(1)" : "scaleY(0)";
     }
+  }
 
-    document.getElementById("prev").addEventListener("click", () => {
-      index = (index - 1 + totalSlides) % totalSlides;
-      showSlide();
-    });
-
-    document.getElementById("next").addEventListener("click", () => {
-      index = (index + 1) % totalSlides;
-      showSlide();
-    });
+  card.addEventListener("click", toggleMenu);
+  icon.addEventListener("click", (e) => {
+    e.stopPropagation(); // evita doble disparo
+    toggleMenu();
+  });
